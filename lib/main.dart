ad79280e61c736d3663ed73337e0594e25b0530e
Edward-Chau <edwardchau517@gmail.com>
2024-10-08 17:03:23 +0800

    1008

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:expense_list_project/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  // SystemChrome.setPreferredOrientations(),
   runApp(const ThemeSetting());
 }
